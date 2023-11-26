d88ae6947a228531314973f095d35021801e13f6
Elisha Kibet <75848566+Elikibs@users.noreply.github.com>
2023-11-26 14:47:26 +0300

    Update looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
+# Assignment 1
+def happy_new_year(num):
+  while num > 0:
+    print(num)
+    num -=1
+  print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+happy_new_year(10)
 
-def fizzbuzz():
-    # code goes here!
-    pass
+# Assignment 2
+def square_integers(list_of_integers):
+  return [num * num for num in list_of_integers]
+
+print(square_integers([1, 2, 3, 4, 5]))
+
+# Assignemnt 3
+def fizz_buz():
+  for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+      print("FizzBuzz")
+    elif num % 3 == 0:
+      print("Fizz")
+    elif num % 5 == 0:
+      print("Buzz")
+    else:
+      print(num)
+
+print(fizz_buz())
